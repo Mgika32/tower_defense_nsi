304a78007645091c214acc9a774e53228709b522
Mgika32 <mickael.guemin@e.rascol.net>
2024-10-18 09:11:23 +0200

    test

--- Player.py
+++ Player.py
@@ -1,2 +1,9 @@
 
-class personnage
+class Player:
+    def __init__(self):
+        self.vie = 100
+        self.ig_money = 200
+        self.gasha_money = 0
+    
+    def take_damage(self, enemy):
+        self.vie -= enemy.damage
