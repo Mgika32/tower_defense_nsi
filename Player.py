513e3b0216eb2dd16cdfcdfd01e7074591e73832
Mgika32 <mickael.guemin@e.rascol.net>
2024-10-18 09:24:30 +0200

    ajout du set money

--- Player.py
+++ Player.py
@@ -4,6 +4,11 @@ class Player:
         self.vie = 100
         self.ig_money = 200
         self.gasha_money = 0
+        self.in_life = True
     
     def take_damage(self, enemy):
         self.vie -= enemy.damage
+    
+    def set_money(self, enemy):
+        self.ig_money += enemy.give_money
+    
